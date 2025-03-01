dd8ab8446da7b7421b1c63a2d1e4c16648644bfc
daxyman <danielfelipegg3005@gmail.com>
2025-02-28 21:53:25 -0500

    exos

---
+++ exercices/exam01/exo01.js
@@ -0,0 +1,21 @@
+const car = {
+    brand: "hyundai",
+    model: "Ioniq 5",
+    year: 2022
+}
+
+console.log(car.brand);
+console.log(car.model);
+console.log(car.year);
+
+const person = {
+    firstname: "daniel" ,
+    lastname: "gonzalez",
+    address: {
+        number: "101",
+        street: "boulevard richelieu",
+        city: "Repentigny"
+    }
+}
+console.log(person.firstname, person.lastname)
+console.log(person.address.number, person.address.street, person.address.city);
